30f41f92f14d681e139859736f4779ad200e2451
yanalasanthoshreddy <santhosh.201316@gmail.com>
2021-03-03 21:29:01 +0530

    initial commit

---
+++ src/main/java/com/emp/application/WowConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.emp.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WowConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WowConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emp/application/WowConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emp.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WowConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
